badb2a5e7fb87730b7a44019b261a4456711f498
shou-watanabe <watanabeshou1520@gmail.com>
2021-08-24 17:43:11 +0900

    [add] sample

---
+++ front/src/components/home/HomeButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-btn
+      :elevation="1"
+      @click="pushButton"
+    >
+      {{ buttonText }}
+    </v-btn>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: "Button",
+  props: {
+    buttonText: {
+      type: String,
+      default: "",
+      required: true
+    }
+  },
+  methods: {
+    pushButton(): void {
+      this.$emit("button-push", "ええええ");
+    }
+  }
+})
+</script>
--- front/src/pages/Home.vue
+++ front/src/pages/Home.vue
@@ -1,16 +1,34 @@
 <template>
-  <hello-world />
+  <div>
+    <HomeButton
+      :buttonText="name"
+      @button-push="buttonPush"
+    />
+    <hello-world />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import HelloWorld from "../components/HelloWorld.vue";
+import HomeButton from "../components/home/HomeButton.vue";
 
 export default Vue.extend({
   name: "Home",
-
   components: {
     HelloWorld,
+    HomeButton
   },
+  data() {
+    return {
+      name: "いいい"
+    }
+  },
+  methods: {
+    buttonPush(newText: string): void {
+      console.log("aaaaa");
+      this.name = newText;
+    }
+  }
 });
 </script>
