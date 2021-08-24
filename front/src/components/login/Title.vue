be32a9d331748d9432c1a58b67b0757e2673cc39
hoso <hosokawa.2000428@gmail.com>
2021-08-25 00:26:49 +0900

    new login

---
+++ front/src/components/login/Title.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<h1>MangaNuma</h1>
+	</div>
+</template>
+
+<script lang="ts">
+	import Vue from 'vue'
+
+	export default Vue.extend({
+		
+	})
+</script>
+
+<style scoped>
+	h1{
+		text-align: center;
+		color: white;
+	}
+</style>
