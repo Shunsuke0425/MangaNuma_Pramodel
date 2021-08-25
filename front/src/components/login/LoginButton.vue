5e4e2ea679c46a781e03b4a53c2c09e4582e2ac7
hoso <hosokawa.2000428@gmail.com>
2021-08-25 10:46:00 +0900

    lint

--- front/src/components/login/LoginButton.vue
+++ front/src/components/login/LoginButton.vue
@@ -1,21 +1,19 @@
 <template>
-	<div>
-		<v-btn elevation="5">ログイン</v-btn>
-	</div>
+  <div>
+    <v-btn elevation="5">ログイン</v-btn>
+  </div>
 </template>
 
 <script lang="ts">
-	import Vue from 'vue'
+import Vue from "vue";
 
-	export default Vue.extend({
-		
-	})
+export default Vue.extend({});
 </script>
 
 <style scoped>
-	div{
-		margin: 0 auto;
-		width: 100%;
-		max-width: 100px;
-	}
+div {
+  margin: 0 auto;
+  width: 100%;
+  max-width: 100px;
+}
 </style>
--- front/src/pages/Login.vue
+++ front/src/pages/Login.vue
@@ -1,48 +1,42 @@
 <template>
   <div class="full-page">
-    <Title
-			class="title-class"	
-		/>
-    <LoginForm
-      :labelName="id"
-		/>
-		<LoginForm
-			:labelName="password"
-		/>
-		<LoginButton />
+    <Title class="title-class" />
+    <LoginForm :labelName="id" />
+    <LoginForm :labelName="password" />
+    <LoginButton />
   </div>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
-	import Title from "../components/login/Title.vue";
-  import LoginForm from "../components/login/LoginForm.vue";
-	import LoginButton from "../components/login/LoginButton.vue";
+import Vue from "vue";
+import Title from "../components/login/Title.vue";
+import LoginForm from "../components/login/LoginForm.vue";
+import LoginButton from "../components/login/LoginButton.vue";
 
-  export default Vue.extend({
-    name: "Login",
-    components: {
-			Title,
-	  	LoginForm,
-			LoginButton
-    },
-		data(){
-			return{
-				id:"userID",
-				password:"password"
-			}
-		}
-  })
+export default Vue.extend({
+  name: "Login",
+  components: {
+    Title,
+    LoginForm,
+    LoginButton,
+  },
+  data() {
+    return {
+      id: "userID",
+      password: "password",
+    };
+  },
+});
 </script>
 
 <style scoped>
-	.full-page{
-		background-color: #8c52ff;
-		width: 100vw;
-		height: 100vh;
-	}
-	.title-class{
-		padding-top: 50px;				/* marginだと上部が白くなってしまった */
-		margin-bottom: 250px;
-	}
+.full-page {
+  background-color: #8c52ff;
+  width: 100vw;
+  height: 100vh;
+}
+.title-class {
+  padding-top: 50px; /* marginだと上部が白くなってしまった */
+  margin-bottom: 250px;
+}
 </style>
