f5852b50717385ff4fb890f8c329296f29880383
shou-watanabe <watanabeshou1520@gmail.com>
2021-08-26 10:25:12 +0900

    [add]ポイント追加のポップアップ追加

---
+++ front/src/components/common/AddPointPopup.vue
@@ -0,0 +1,48 @@
+<template>
+  <BasePopup ref="popup" :title="title" :width="260" persisten>
+    <template #actions>
+      <v-row justify="center">
+        <v-btn
+          color="yellow"
+          width="180px"
+          height="50px"
+          class="mb-4"
+          @click="closeDialog"
+        >
+          <div class="pl-3">OK</div>
+        </v-btn>
+      </v-row>
+    </template>
+  </BasePopup>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import BasePopup from "../common/BasePopup.vue";
+
+export default Vue.extend({
+  name: "AddPointPopup",
+  components: {
+    BasePopup,
+  },
+  computed: {
+    title(): string {
+      return `${this.getPoint}ポイントゲット!!`;
+    },
+  },
+  data() {
+    return {
+      getPoint: 0,
+    };
+  },
+  methods: {
+    openDialog(point: number): void {
+      this.getPoint = point;
+      (this.$refs.popup as InstanceType<typeof BasePopup>).openDialog();
+    },
+    closeDialog(): void {
+      (this.$refs.popup as InstanceType<typeof BasePopup>).closeDialog();
+    },
+  },
+});
+</script>
--- front/src/pages/BuyPoint.vue
+++ front/src/pages/BuyPoint.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-app>
-    <BaseContainer>
+  <div>
+    <BaseContainer :point="userPoint">
       <template #mainContents>
         <h1>ポイント購入</h1>
 
@@ -18,35 +18,58 @@
         <v-row justify="center">
           <v-btn elevation="2" @click="getPoint">確定</v-btn>
         </v-row>
-        <h2 class="text-center mt-16">現在: {{ point }}P</h2>
+        <h2 class="text-center mt-16">現在: {{ userPoint }}P</h2>
       </template>
     </BaseContainer>
-  </v-app>
+    <AddPointPopup ref="addPointPopup" />
+    <CodeMissPopup ref="codeMissPopup" />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import BaseContainer from "../components/common/BaseContainer.vue";
 import PointCard from "../components/buyPoint/PointCard.vue";
+import AddPointPopup from "../components/common/AddPointPopup.vue";
+import CodeMissPopup from "../components/buyPoint/CodeMissPopup.vue";
 
 export default Vue.extend({
   name: "Home",
   components: {
     BaseContainer,
+    AddPointPopup,
     PointCard,
+    CodeMissPopup,
+  },
+  computed: {
+    userPoint(): number {
+      return this.$store.state.point;
+    },
   },
   data() {
     return {
-      point: 100,
       addNumber: "",
     };
   },
   methods: {
     getPoint(): void {
       if (this.addNumber == "1111") {
-        this.point += 200;
+        this.openAddPointDialog(200);
+        this.$store.commit("addPoint", 200);
         this.addNumber = "";
-      }
+      } else this.openCodeMissDialog();
+      console.log("getPoint");
+    },
+    openAddPointDialog(point: number): void {
+      (
+        this.$refs.addPointPopup as InstanceType<typeof AddPointPopup>
+      ).openDialog(point);
+    },
+    openCodeMissDialog(): void {
+      console.log("open");
+      (
+        this.$refs.codeMissPopup as InstanceType<typeof CodeMissPopup>
+      ).openDialog();
     },
   },
 });
